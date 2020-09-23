a15cfacadd327ac78989df65541aeedb732ad7f4
willsan0723 <william.santee@gmail.com>
2020-09-22 18:53:03 -0600

    dynamically generated a HTML file. refactored code so generation is in a separate file.

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+const profileDataArgs = process.argv.slice(2);
+const [name, github] = profileDataArgs;
 
-const printProfileData = profileDataArr => {
-    for (let i=0; i<profileDataArr.length; i += 1){
-        console.log(profileDataArr[i]);
-    }
-    console.log('================');
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
+fs.writeFile('./index.html', generatePage(name, github), err => {
+    if (err) throw new Error(err);
 
-printProfileData(profileDataArgs);
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
