d87c9d98c1ea295024ab5c647f1a38a6201887d2
willsan0723 <william.santee@gmail.com>
2020-09-24 17:52:30 -0600

    need to figure out why this isn't working. going back to other branch

--- app.js
+++ app.js
@@ -1,14 +1,7 @@
 const inquirer = require('inquirer');
-// const fs = require('fs');
-// const generatePage = require('./src/page-template');
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
-// const pageHTML = generatePage(name, github);
-
-// fs.writeFile('./index.html', pageHTML, err => {
-//     if (err) throw err;
-
-//     console.log('Portfolio complete! Checout out index.html to see the output!');
-// })
 
 const promptUser = () => {
     return inquirer.prompt([
@@ -141,7 +134,13 @@ const promptProject = portfolioData => {
  promptUser()
     .then(promptProject)
     .then(portfolioData => {
-        console.log(portfolioData)
+        const pageHTML = generatePage(portfolioData);
+        
+        // fs.writeFile('./index.html', pageHTML, err => {  
+        //     if (err) throw err;
+
+        // console.log('Page created! Check out index.html in this directory to see it!');
+        // })
     });
 
  
